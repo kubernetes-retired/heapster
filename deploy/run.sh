aaadf41bebb9adb04424bfe76ec05cb5e88b63b3
Vishnu Kannan <vishnuk@google.com>
2014-08-27 07:10:08 +0000

    Fix run script in docker image.

--- deploy/run.sh
+++ deploy/run.sh
@@ -2,21 +2,21 @@
 
 set -e
 
-if [ ! ${KUBE_MASTER:?} ]; then
+if [ -z $KUBE_MASTER ]; then
     echo "KUBE_MASTER env variable is not set"
     exit 1
 fi
-if [ ! ${KUBE_MASTER_AUTH:?} ]; then
+if [ -z $KUBE_MASTER_AUTH ]; then
     echo "KUBE_MASTER_AUTH env variable is not set"
     exit 1
 fi
 
 # Check if InfluxDB service is running
-if [ ${INFLUX_MASTER_SERVICE_PORT:?} ]; then
+if [ ! -z $INFLUX_MASTER_SERVICE_PORT ]; then
 # TODO(vishh): add support for passing in user name and password.    
     echo "InfluxDB service discovered. Starting heapster with InfluxDb sink running at ${SERVICE_HOST}:${INFLUX_MASTER_SERVICE_PORT}"
     /usr/bin/heapster --kubernetes_master ${KUBE_MASTER} --kubernetes_master_auth ${KUBE_MASTER_AUTH} --sink influxdb --sink_influxdb_host "${SERVICE_HOST}:${INFLUX_MASTER_SERVICE_PORT}"
-elif [ ${INFLUXDB_HOST:?} ]; then
+elif [ ! -z $INFLUXDB_HOST ]; then
     echo "InfluxDB host specified in commandline. Starting heapster with InfluxDb sink running at ${INFLUXDB_HOST}"
     /usr/bin/heapster --kubernetes_master ${KUBE_MASTER} --kubernetes_master_auth ${KUBE_MASTER_AUTH} --sink influxdb --sink_influxdb_host ${INFLUXDB_HOST}
 else
