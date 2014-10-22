30a27b6d751582cf54142c406d746dc045039e12
Victor Marmol <vmarmol@google.com>
2014-10-22 09:39:53 -0700

    Use godep for build and print commands in run.

--- deploy/build.sh
+++ deploy/build.sh
@@ -4,4 +4,4 @@ set -e
 set -x
 
 # Statically build cAdvisor from source and stage it.
-go build -a github.com/GoogleCloudPlatform/heapster
+godep go build -a github.com/GoogleCloudPlatform/heapster
