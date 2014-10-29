978a184ae0cbaa04e910875a9321e771071176d7
Vishnu Kannan <vishnuk@google.com>
2014-10-29 00:06:06 +0000

    Update influxdb docker image.

--- influx-grafana/influxdb/run.sh
+++ influx-grafana/influxdb/run.sh
@@ -1,18 +1,48 @@
 #!/bin/bash
 
-set -e
+set -m
 
-/usr/bin/influxdb -config=/opt/influxdb/shared/config.toml &
-if [ ! -z "${DEFAULT_DB}" ]; then    
-    until false
-    do
-	result=$(curl -X POST 'http://localhost:8086/db?u=root&p=root' -d '{"name": ${DEFAULT_DB}}')
-	echo result
-	if [[ result == *exists* ]]; then
-	    break
-	fi	    
-	sleep 5
-	echo "Attempting to create db $(DEFAULT_DB) again"
-    done
+CONFIG_FILE="/config/config.toml"
+API_URL="http://localhost:8086"
+SHARD_CONFIG="/config/shardSpace.json"
+if [ "${PRE_CREATE_DB}" == "**None**" ]; then
+    unset PRE_CREATE_DB
 fi
-wait
+if [ -n "${PRE_CREATE_DB}" ]; then
+    echo "=> About to create the following database: ${PRE_CREATE_DB}"
+    if [ -f "/data/.pre_db_created" ]; then
+        echo "=> Database had been created before, skipping ..."
+    else
+        echo "=> Starting InfluxDB ..."
+        exec /usr/bin/influxdb -config=${CONFIG_FILE} &
+        PASS=${INFLUXDB_INIT_PWD:-root}
+        arr=$(echo ${PRE_CREATE_DB} | tr ";" "\n")
+
+        #wait for the startup of influxdb
+        RET=1
+        while [[ RET -ne 0 ]]; do
+            echo "=> Waiting for confirmation of InfluxDB service startup ..."
+            sleep 3 
+            curl -k ${API_URL}/ping 2> /dev/null
+            RET=$?
+        done
+        echo ""
+
+        for x in $arr
+        do
+            echo "=> Creating database: ${x}"
+            curl -s -k -X POST -d "{\"name\":\"${x}\"}" $(echo ${API_URL}'/db?u=root&p='${PASS}) --data-binary @${SHARD_CONFIG}
+        done
+        echo ""
+
+        touch "/data/.pre_db_created"
+        fg
+        exit 0
+    fi
+else
+    echo "=> No database need to be pre-created"
+fi
+
+echo "=> Starting InfluxDB ..."
+
+exec /usr/bin/influxdb -config=${CONFIG_FILE}
