f1885a194da8b44093e63909544508d89da1221a
Vishnu Kannan <vishnuk@google.com>
2015-01-08 23:16:52 +0000

    Adding a test for heapster in GCE based kubernetes.

--- integration/framework.go
+++ integration/framework.go
@@ -1,3 +1,17 @@
+// Copyright 2014 Google Inc. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package integration
 
 import (
@@ -9,20 +23,22 @@ import (
 	"path"
 	"strings"
 	"testing"
+	"time"
 
-	"github.com/golang/glog"
 	kube_client "github.com/GoogleCloudPlatform/kubernetes/pkg/client"
+	kube_clientauth "github.com/GoogleCloudPlatform/kubernetes/pkg/clientauth"
+	"github.com/golang/glog"
 )
 
 type kubeFramework interface {
-	// Cleanup cluster
-	Cleanup() error
-
 	// The testing.T used by the framework and the current test.
 	T() *testing.T
 
 	// Kube client
 	Client() *kube_client.Client
+
+	// Run kubectl.sh command
+	RunKubectlCmd(cmd ...string) (string, error)
 }
 
 type realKubeFramework struct {
@@ -42,7 +58,11 @@ type realKubeFramework struct {
 
 const imageUrlTemplate = "https://github.com/GoogleCloudPlatform/kubernetes/releases/download/v%s/kubernetes.tar.gz"
 
-var workDir = flag.String("work_dir", "/tmp/heapster_test", "Filesystem path where test files will be stored. Files will persist across runs to speed up tests.")
+var (
+	authConfig         = flag.String("auth_config", os.Getenv("HOME")+"/.kubernetes_auth", "Path to the auth info file.")
+	useExistingCluster = flag.Bool("use_existing_cluster", false, "when true uses an existing kube cluster. A cluster needs to exist.")
+	workDir            = flag.String("work_dir", "/tmp/heapster_test", "Filesystem path where test files will be stored. Files will persist across runs to speed up tests.")
+)
 
 func exists(path string) bool {
 	if _, err := os.Stat(path); err != nil {
@@ -56,40 +76,47 @@ const pathToGCEConfig = "cluster/gce/config-default.sh"
 func disableClusterMonitoring(kubeBaseDir string) error {
 	kubeConfigFilePath := path.Join(kubeBaseDir, pathToGCEConfig)
 	input, err := ioutil.ReadFile(kubeConfigFilePath)
-        if err != nil {
+	if err != nil {
 		return err
-        }
+	}
 
-        lines := strings.Split(string(input), "\n")
+	lines := strings.Split(string(input), "\n")
 
-        for i, line := range lines {
+	for i, line := range lines {
 		if strings.Contains(line, "ENABLE_CLUSTER_MONITORING") {
-                        lines[i] = "ENABLE_CLUSTER_MONITORING=false"
-                }
-        }
-        output := strings.Join(lines, "\n")
-        err = ioutil.WriteFile(kubeConfigFilePath, []byte(output), 0644)
-        if err != nil {
-		return err
-        }
-	return nil
+			lines[i] = "ENABLE_CLUSTER_MONITORING=false"
+		} else if strings.Contains(line, "NUM_MINIONS=") {
+			lines[i] = "NUM_MINIONS=2"
+		}
+	}
+	output := strings.Join(lines, "\n")
+	return ioutil.WriteFile(kubeConfigFilePath, []byte(output), 0644)
+}
+
+func runKubeClusterCommand(kubeBaseDir, command string) ([]byte, error) {
+	cmd := exec.Command(path.Join(kubeBaseDir, "cluster", command))
+	glog.V(2).Infof("about to run %v", cmd)
+	return cmd.CombinedOutput()
 }
 
 func setupNewCluster(kubeBaseDir string) error {
-	out, err := exec.Command(path.Join(kubeBaseDir, "cluster", "kube-up.sh")).CombinedOutput()
+	out, err := runKubeClusterCommand(kubeBaseDir, "kube-up.sh")
 	if err != nil {
-		glog.Infof("failed to bring up cluster. Output: %s\nErr: %q", out, err)
+		glog.Errorf("failed to bring up cluster - %q\n%s", err, out)
 		return fmt.Errorf("failed to bring up cluster - %q", err)
 	}
+
 	return nil
 }
 
 func tearDownCluster(kubeBaseDir string) error {
-	out, err := exec.Command(path.Join(kubeBaseDir, "cluster", "kube-down.sh")).CombinedOutput()
+	glog.V(1).Info("Bringing down any existing kube cluster")
+	out, err := runKubeClusterCommand(kubeBaseDir, "kube-down.sh")
 	if err != nil {
-		glog.V(1).Infof("failed to tear down cluster. Output: %s\nErr: %q", out, err)
-		return fmt.Errorf("failed to tear down cluster - %q", err)
+		glog.Errorf("failed to tear down cluster - %q\n%s", err, out)
+		return fmt.Errorf("failed to tear down kube cluster - %q", err)
 	}
+
 	return nil
 }
 
@@ -102,11 +129,11 @@ func getMasterIP() (string, error) {
 	for _, line := range strings.Split(string(out), "\n") {
 		if strings.Contains(line, "natIP") {
 			externalIPLine := strings.Split(line, ":")
-			glog.V(1).Infof("Master external IP line %v", externalIPLine)
 			return strings.TrimSpace(externalIPLine[1]), nil
 		}
 	}
-	return "", fmt.Errorf("External IP not found for the master. 'gcloud compute instances list kubernetes-master' returned %s", out)
+
+	return "", fmt.Errorf("external IP not found for the master. 'gcloud compute instances list kubernetes-master' returned %s", out)
 }
 
 func downloadRelease(workDir, version string) error {
@@ -118,14 +145,52 @@ func downloadRelease(workDir, version string) error {
 
 	// Download kube code and store it in a temp dir.
 	if err := exec.Command("wget", downloadUrl, "-O", downloadPath).Run(); err != nil {
-		return fmt.Errorf("failed to wget kubernetes release @ %s - %q", downloadUrl, err)
+		return fmt.Errorf("failed to wget kubernetes release @ %q - %v", downloadUrl, err)
 	}
 
 	// Un-tar kube release.
 	if err := exec.Command("tar", "-xf", downloadPath, "-C", workDir).Run(); err != nil {
-		return fmt.Errorf("failed to un-tar kubernetes release at %s - %q", downloadPath, err)
+		return fmt.Errorf("failed to un-tar kubernetes release at %q - %v", downloadPath, err)
 	}
 	return nil
+}
+
+func getKubeClient() (*kube_client.Client, error) {
+	masterIP, err := getMasterIP()
+	if err != nil {
+		return nil, err
+	}
+	glog.V(1).Infof("Kubernetes master IP is %s", masterIP)
+
+	config := kube_client.Config{
+		Host:    "https://" + masterIP,
+		Version: "v1beta1",
+	}
+
+	auth, err := kube_clientauth.LoadFromFile(*authConfig)
+	if err != nil {
+		return nil, fmt.Errorf("error loading auth - %q", err)
+	}
+	config, err = auth.MergeWithConfig(config)
+	if err != nil {
+		return nil, fmt.Errorf("error creating client - %q", err)
+	}
+
+	kubeClient, err := kube_client.New(&config)
+	if err != nil {
+		return nil, fmt.Errorf("error creating client - %q", err)
+	}
+
+	return kubeClient, nil
+}
+
+func validateCluster(baseDir string) bool {
+	out, err := runKubeClusterCommand(baseDir, "validate-cluster.sh")
+	if err != nil {
+		glog.V(1).Infof("cluster validation failed - %q\n %s", err, out)
+		return false
+	}
+	return true
 }
 
 func newKubeFramework(t *testing.T, version string) (kubeFramework, error) {
@@ -134,12 +199,16 @@ func newKubeFramework(t *testing.T, version string) (kubeFramework, error) {
 		t.Skip("Skipping framework test in short mode")
 	}
 
+	if !exists(*authConfig) {
+		return nil, fmt.Errorf("failed to find kube auth file: %s", *authConfig)
+	}
+
 	// Create a temp dir to store the kube release files.
 	tempDir := path.Join(*workDir, version)
 	if !exists(tempDir) {
 		if err := os.MkdirAll(tempDir, 0700); err != nil {
 			return nil, fmt.Errorf("failed to create a temp dir at %s - %q", tempDir, err)
-		}	
+		}
 		glog.V(1).Infof("Successfully setup work dir at %s", tempDir)
 	}
 
@@ -156,51 +225,62 @@ func newKubeFramework(t *testing.T, version string) (kubeFramework, error) {
 	if err := disableClusterMonitoring(kubeBaseDir); err != nil {
 		return nil, fmt.Errorf("failed to disable cluster monitoring in kube cluster config - %q", err)
 	}
+	glog.V(1).Info("Disabled cluster monitoring")
 
-	// Teardown any pre-existing cluster - This can happen due to a failed test or bad release.
-	if err := tearDownCluster(kubeBaseDir); err != nil {
-		return nil, err
+	if !*useExistingCluster {
+		// TODO(vishh): Do a kube-push if a cluster already exists and bring down monitoring jobs.
+		// Teardown any pre-existing cluster - This can happen due to a failed test or bad release.
+		// Ignore teardown errors since a cluster may not even exist.
+		tearDownCluster(kubeBaseDir)
+
+		// Setup kube cluster
+		glog.V(1).Infof("Setting up new kubernetes cluster version: %s", version)
+		if err := setupNewCluster(kubeBaseDir); err != nil {
+			// Cluster setup failed for some reason.
+			// Attempting to validate the cluster to see if it failed in the validate phase.
+			sleepDuration := 10 * time.Second
+			var clusterReady bool = false
+			for i := 0; i < int(time.Minute/sleepDuration); i++ {
+				if !validateCluster(kubeBaseDir) {
+					glog.Infof("Retry validation after %v seconds.", sleepDuration/time.Second)
+					time.Sleep(sleepDuration)
+				} else {
+					clusterReady = true
+					break
+				}
+			}
+			if !clusterReady {
+				return nil, fmt.Errorf("failed to setup cluster - %q", err)
+			}
+		}
+
+		glog.V(1).Infof("Successfully setup new kubernetes cluster version %s", version)
 	}
-
-	// Setup kube cluster 
-	if err := setupNewCluster(kubeBaseDir); err != nil {
-		return nil, fmt.Errorf("failed to setup kube cluster - %q", err)	
-	}
-
-	glog.V(1).Infof("Successfully setup new kubernetes cluster version %s", version)
 
 	// Setup kube client
-	masterIP, err := getMasterIP()
+	kubeClient, err := getKubeClient()
 	if err != nil {
 		return nil, err
 	}
-	glog.V(1).Infof("Kubernetes master IP is %s", masterIP)
-
-	kubeClient := kube_client.NewOrDie(&kube_client.Config{
-		Host:     "http://" + masterIP,
-		Version:  "v1beta1",
-		Insecure: true,
-	})
 
 	return &realKubeFramework{
 		kubeClient: kubeClient,
-		t:        t,
-		baseDir: kubeBaseDir,
-	}, nil	
+		t:          t,
+		baseDir:    kubeBaseDir,
+	}, nil
 }
 
 func (self *realKubeFramework) T() *testing.T {
 	return self.t
 }
 
-func (self *realKubeFramework) Cleanup() error {
-	if err := tearDownCluster(self.baseDir); err != nil {
-		return err
-	}
-
-	return nil
-}
-
 func (self *realKubeFramework) Client() *kube_client.Client {
 	return self.kubeClient
+}
+
+func (self *realKubeFramework) RunKubectlCmd(args ...string) (string, error) {
+	cmd := exec.Command(path.Join(self.baseDir, "cluster", "kubectl.sh"), args...)
+	glog.V(2).Infof("about to run cmd: %+v", cmd)
+	out, err := cmd.CombinedOutput()
+	return string(out), err
 }
--- integration/kube_test.go
+++ integration/kube_test.go
@@ -1,20 +1,132 @@
+// Copyright 2014 Google Inc. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package integration
 
 import (
 	"flag"
+	"fmt"
 	"strings"
 	"testing"
-	
-	"github.com/stretchr/testify/assert"
+	"time"
+
+	kube_api "github.com/GoogleCloudPlatform/kubernetes/pkg/api"
+	"github.com/GoogleCloudPlatform/kubernetes/pkg/labels"
+	"github.com/golang/glog"
+	influxdb "github.com/influxdb/influxdb/client"
+	"github.com/stretchr/testify/require"
 )
 
-var kubeVersions = flag.String("kube_versions", "0.7.2", "Comma separated list of kube versions to test against")
+var (
+	influxdbController          = "monitoring-influxGrafanaController"
+	heapsterController          = "monitoring-heapsterController"
+	influxdbLabels              = map[string]string{"name": "influxGrafana"}
+	heapsterLabels              = map[string]string{"name": "heapster"}
+	kubeVersions                = flag.String("kube_versions", "0.7.2,0.8.0", "Comma separated list of kube versions to test against")
+	heapsterManifestFile        = flag.String("heapster_controller", "../deploy/heapster-controller.js", "Path to heapster replication controller file.")
+	influxdbGrafanaManifestFile = flag.String("influxdb_grafana_controller", "../deploy/influxdb-grafana-controller.js", "Path to Influxdb-Grafana replication controller file.")
+	influxdbServiceFile         = flag.String("influxdb_service", "../deploy/influxdb-service.json", "Path to Inlufxdb service file.")
+)
 
-func TestHeapsterInfluxDBWorks(t * testing.T) {
+func waitUntilPodRunning(fm kubeFramework, podLabels map[string]string, timeout time.Duration) (string, error) {
+	podsInterface := fm.Client().Pods(kube_api.NamespaceDefault)
+	for i := 0; i < int(timeout/time.Second); i++ {
+		selector := labels.Set(podLabels).AsSelector()
+		podList, err := podsInterface.List(selector)
+		if err != nil {
+			glog.V(1).Info(err)
+			return "", err
+		}
+		if len(podList.Items) != 1 {
+			glog.V(1).Info(err)
+			return "", fmt.Errorf("found %d pod with labels %v", len(podList.Items), podLabels)
+		}
+		podSpec := podList.Items[0]
+		glog.V(2).Infof("%+v", podSpec)
+		if podSpec.Status.Phase == kube_api.PodRunning {
+			return podSpec.Status.HostIP, nil
+		}
+		time.Sleep(time.Second)
+	}
+	return "", fmt.Errorf("pod not in running state after %d seconds", timeout/time.Second)
+}
+
+func updateReplicas(fm kubeFramework, name string, count int) error {
+	rcInterface := fm.Client().ReplicationControllers(kube_api.NamespaceDefault)
+	controller, err := rcInterface.Get(name)
+	if err == nil {
+		controller.Spec.Replicas = 0
+		if _, rerr := rcInterface.Update(controller); rerr != nil {
+			return rerr
+		}
+	}
+	glog.Errorf("updateReplicas failed for controller %+v with error: %q", controller, err)
+	return err
+}
+
+func deletePods(fm kubeFramework) {
+	// TODO(vishh): Use kubecfg.sh instead. Native APIs aren't working.
+	if err := updateReplicas(fm, influxdbController, 0); err != nil {
+		glog.Errorf("failed to bring down the number of replicas for influxdb")
+	}
+	if err := updateReplicas(fm, heapsterController, 0); err != nil {
+		glog.Errorf("failed to bring down the number of replicas for heapster")
+	}
+	_, _ = fm.RunKubectlCmd("delete", "-f", *heapsterManifestFile)
+	_, _ = fm.RunKubectlCmd("delete", "-f", *influxdbGrafanaManifestFile)
+	_, _ = fm.RunKubectlCmd("delete", "-f", *influxdbServiceFile)
+}
+
+func TestHeapsterInfluxDBWorks(t *testing.T) {
 	kubeVersionsList := strings.Split(*kubeVersions, ",")
 	for _, kubeVersion := range kubeVersionsList {
 		fm, err := newKubeFramework(t, kubeVersion)
-		assert.NoError(t, err)
-		assert.NoError(t, fm.Cleanup())
+		require.NoError(t, err, "failed to create kube framework")
+
+		deletePods(fm)
+		out, err := fm.RunKubectlCmd("create", "-f", *influxdbGrafanaManifestFile)
+		require.NoError(t, err, "failed to create Influxdb-grafana pod ", out)
+		out, err = fm.RunKubectlCmd("create", "-f", *influxdbServiceFile)
+		require.NoError(t, err, "failed to create Influxdb service ", out)
+		out, err = fm.RunKubectlCmd("create", "-f", *heapsterManifestFile)
+		require.NoError(t, err, "failed to create heapster pod ", out)
+
+		glog.V(1).Info("waiting for pods to be running")
+		influxdbHostIP, err := waitUntilPodRunning(fm, influxdbLabels, 10*time.Minute)
+		require.NoError(t, err, "influxdb pod is not running")
+		_, err = waitUntilPodRunning(fm, heapsterLabels, 1*time.Minute)
+		require.NoError(t, err, "heapster pod is not running")
+
+		glog.V(1).Infof("checking if data exists in influxdb running at %s", influxdbHostIP)
+		config := &influxdb.ClientConfig{
+			Host: influxdbHostIP + ":8086",
+			// TODO(vishh): Infer username and pw from the Pod spec.
+			Username: "root",
+			Password: "root",
+			Database: "k8s",
+			IsSecure: false,
+		}
+		influxdbClient, err := influxdb.NewClient(config)
+		require.NoError(t, err, "failed to create influxdb client")
+
+		data, err := influxdbClient.Query("select * from stats limit 1", influxdb.Second)
+		require.NoError(t, err, "failed to query data from 'stats' table in Influxdb")
+		require.NotEmpty(t, data, "'stats' table does not contain any data")
+
+		data, err = influxdbClient.Query("select * from machine limit 1", influxdb.Second)
+		require.NoError(t, err, "failed to query data from 'machine' table in Influxdb")
+		require.NotEmpty(t, data, "'machine' table does not contain any data")
+		glog.V(1).Info("HeapsterInfluxDB test passed")
 	}
 }
