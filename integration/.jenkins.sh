560d7dc7e7f1105c11fc8b23ff22bebb61537c49
Vishnu Kannan <vishnuk@google.com>
2015-01-16 20:08:44 +0000

    .

--- integration/.jenkins.sh
+++ integration/.jenkins.sh
@@ -7,6 +7,5 @@ deploy/build-test.sh \
 && influx-grafana/grafana/build-test.sh \
 && influx-grafana/influxdb/build-test.sh \
 && pushd integration \
-&& cd integration \
 && godep go test -a -v --vmodule=*=1 --timeout=30m --kube_versions="0.7.2,0.8.0" ./... \
 && popd
