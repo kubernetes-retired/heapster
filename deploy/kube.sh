a88713287a68eafe839b6f5b328522a62a23d3a1
Vishnu Kannan <vishnuk@google.com>
2015-01-21 22:25:17 +0000

    Make heapster discover InfluxDB service using DNS. Environment variables based service discovery is still the first choice. Updated service config to use port 80 since kube dns seems to require that. Fixed initialization errors in kube source.

--- deploy/kube.sh
+++ deploy/kube.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-set -o errexit
-
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
 start() {
@@ -13,13 +11,15 @@ start() {
 }
 
 stop() {
-    if kubecfg.sh resize monitoring-influxGrafanaController 0 &> /dev/null \
-      && kubecfg.sh resize monitoring-heapsterController 0 &> /dev/null \
-      && kubectl.sh delete -f "$DIR/" &> /dev/null; then 
-      echo "heapster pods have been removed."
-    else
-      echo "failed to remove heapster pods"
-    fi
+  kubecfg.sh resize monitoring-influxGrafanaController 0 > /dev/null || true
+  kubecfg.sh resize monitoring-heapsterController 0 > /dev/null || true
+  # wait for the pods to disappear.
+  while kubectl.sh get pods -l "name=influxGrafana" -o template -t {{range.items}}{{.id}}:{{end}} | grep -c . &> /dev/null \
+    || kubectl.sh get pods -l "name=heapster" -o template -t {{range.items}}{{.id}}:{{end}} | grep -c . &> /dev/null; do
+    sleep 2
+  done
+  kubectl.sh delete -f "$DIR/" &> /dev/null || true
+  echo "heapster pods have been removed."
 }
 
 case "$1" in
