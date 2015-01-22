dcecd7cadc68b41a63c9a4a560547e1445cc18f8
Vishnu Kannan <vishnuk@google.com>
2015-01-22 18:34:32 +0000

    Update integration tests to test kube version 0.9.0 only since previous versions are incompatible with heapster moving forward.

--- integration/.jenkins.sh
+++ integration/.jenkins.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 set -x
+SUPPORTED_KUBE_VERSIONS="0.9.0"
+
 export GOPATH="$JENKINS_HOME/workspace/project"
 export GOBIN="$GOPATH/bin"
 
@@ -7,5 +9,5 @@ deploy/build-test.sh \
 && influx-grafana/grafana/build-test.sh \
 && influx-grafana/influxdb/build-test.sh \
 && pushd integration \
-&& godep go test -a -v --vmodule=*=1 --timeout=30m github.com/GoogleCloudPlatform/heapster/integration/... \
+&& godep go test -a -v --vmodule=*=1 --timeout=30m --kube_versions=$SUPPORTED_KUBE_VERSIONS github.com/GoogleCloudPlatform/heapster/integration/... \
 && popd
