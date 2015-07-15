kubernetes-retired/heapster

commit 67415bd031b1191e0fa0a9902d6d1c8e1cf47b73

Use a bytes.Buffer for externalSinkManager.DebugInfo()

sinks/external.go
@@ -15,6 +15,7 @@
 package sinks
 
 import (
+	"bytes"
 	"fmt"
 	"strings"
 	"sync"
@@ -101,27 +102,28 @@ func (self *externalSinkManager) Store(input interface{}) error {
 }
 
 func (self *externalSinkManager) DebugInfo() string {
-	desc := "External Sinks\n"
+	b := &bytes.Buffer{}
+	fmt.Fprintln(b, "External Sinks")
 
 	// Add metrics being exported.
-	desc += "\tExported metrics:\n"
+	fmt.Fprintln(b, "\tExported metrics:")
 	for _, supported := range sink_api.SupportedStatMetrics() {
-		desc += fmt.Sprintf("\t\t%s: %s", supported.Name, supported.Description)
+		fmt.Fprintln(b, "\t\t%s: %s", supported.Name, supported.Description)
 	}
 
 	// Add labels being used.
-	desc += "\n\tExported labels:\n"
+	fmt.Fprintln(b, "\tExported labels:")
 	for _, label := range sink_api.SupportedLabels() {
-		desc += fmt.Sprintf("\t\t%s: %s", label.Key, label.Description)
+		fmt.Fprintln(b, "\t\t%s: %s", label.Key, label.Description)
 	}
-	desc += "\n\tExternal Sinks:\n"
+	fmt.Fprintln(b, "\tExternal Sinks:")
 	self.sinkMutex.RLock()
 	defer self.sinkMutex.RUnlock()
 	for _, externalSink := range self.externalSinks {
-		desc += fmt.Sprintf("\n\t\t%s", externalSink.DebugInfo())
+		fmt.Fprintln(b, "\t\t%s", externalSink.DebugInfo())
 	}
 
-	return desc
+	return b.String()
 }
 
 // inSlice returns whether an external sink is part of a set (list) of sinks

commit 2ee936947d3e595e823468cf4d5c12771da0b1a4

Fix fmt format issue introduced in 67415bd031b

sinks/external.go
@@ -106,19 +106,19 @@ func (self *externalSinkManager) DebugInfo() string {
 	// Add metrics being exported.
 	fmt.Fprintln(b, "\tExported metrics:")
 	for _, supported := range sink_api.SupportedStatMetrics() {
-		fmt.Fprintln(b, "\t\t%s: %s", supported.Name, supported.Description)
+		fmt.Fprintf(b, "\t\t%s: %s\n", supported.Name, supported.Description)
 	}
 
 	// Add labels being used.
 	fmt.Fprintln(b, "\tExported labels:")
 	for _, label := range sink_api.SupportedLabels() {
-		fmt.Fprintln(b, "\t\t%s: %s", label.Key, label.Description)
+		fmt.Fprintf(b, "\t\t%s: %s\n", label.Key, label.Description)
 	}
 	fmt.Fprintln(b, "\tExternal Sinks:")
 	self.sinkMutex.RLock()
 	defer self.sinkMutex.RUnlock()
 	for _, externalSink := range self.externalSinks {
-		fmt.Fprintln(b, "\t\t%s", externalSink.DebugInfo())
+		fmt.Fprintf(b, "\t\t%s\n", externalSink.DebugInfo())
 	}
 
 	return b.String()