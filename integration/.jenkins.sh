6f77a7e8b5a0d312570bad49b4d4460532565f70
Daniel Martí <mvdan@mvdan.cc>
2015-09-28 15:45:01 -0700

    Remove GoogleCloudPlatform/heapster moving
    Fixed the workspace directory in the Jenkins config to be src/k8s.io/heapster,
    so the clone is already set up correctly in the GOPATH.

--- integration/.jenkins.sh
+++ integration/.jenkins.sh
@@ -6,10 +6,6 @@ export GOPATH="$JENKINS_HOME/workspace/project"
 export GOBIN="$GOPATH/bin"
 export PATH="$GOBIN:$PATH"
 
-rm -rf $GOPATH/src/k8s.io
-mkdir -p $GOPATH/src/k8s.io
-mv $GOPATH/src/github.com/GoogleCloudPlatform/heapster $GOPATH/src/k8s.io/heapster
-
 if ! git diff --name-only origin/master | grep -c -E "*.go|*.sh|.*yaml" &> /dev/null; then
   echo "This PR does not touch files that require integration testing. Skipping integration tests!"
   exit 0
