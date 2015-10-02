b300bcdccc7f5cb9dc5ceef8034ea624bcd5b899
Vishnu Kannan <vishnuk@google.com>
2015-10-02 13:04:00 -0700

    Update integration test files to avoid dir moves for the sake of vanity paths.

--- integration/.jenkins.sh
+++ integration/.jenkins.sh
@@ -6,16 +6,12 @@ export GOPATH="$JENKINS_HOME/workspace/project"
 export GOBIN="$GOPATH/bin"
 export PATH="$GOBIN:$PATH"
 
-rm -rf $GOPATH/src/k8s.io
-mkdir -p $GOPATH/src/k8s.io
-mv $GOPATH/src/github.com/GoogleCloudPlatform/heapster $GOPATH/src/k8s.io/heapster
-
 if ! git diff --name-only origin/master | grep -c -E "*.go|*.sh|.*yaml" &> /dev/null; then
   echo "This PR does not touch files that require integration testing. Skipping integration tests!"
   exit 0
 fi
 
-SUPPORTED_KUBE_VERSIONS="1.0.1"
+SUPPORTED_KUBE_VERSIONS="1.0.6"
 TEST_NAMESPACE="default"
 
 make test-unit
