9ce7800c12dbd72ca688be08ac2937f152e1b6c3
Marcin Wielgus <mwielgus@google.com>
2016-02-25 15:31:07 +0100

    v0.20.0-alpha8

--- Makefile
+++ Makefile
@@ -1,6 +1,6 @@
 all: build
 
-TAG = v0.20.0-alpha7
+TAG = v0.20.0-alpha8
 PREFIX = gcr.io/google_containers
 FLAGS = 
 
--- version/version.go
+++ version/version.go
@@ -19,4 +19,4 @@ package version
 // Increment major number for new feature additions and behavioral changes.
 // Increment minor number for bug fixes and performance enhancements.
 // Increment patch number for critical fixes to existing releases.
-const HeapsterVersion = "0.20.0-alpha7"
+const HeapsterVersion = "0.20.0-alpha8"
