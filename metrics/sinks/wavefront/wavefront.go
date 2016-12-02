85e665487118f27a9a78c5b2a894c0110301729d
Evan Pease <evan.pease@gmail.com>
2016-12-02 14:03:51 -0800

    corrected license

--- metrics/sinks/wavefront/wavefront.go
+++ metrics/sinks/wavefront/wavefront.go
@@ -1,4 +1,4 @@
-// Copyright 2016 Wavefront Inc. All Rights Reserved.
+// Copyright 2015 Google Inc. All Rights Reserved.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -11,7 +11,7 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-// Usage: ./heapster --source=kubernetes:http://192.168.64.2:8080?inClusterConfig=false --sink=wavefront:wavefront-proxy:2878?clusterName=k8s-cluster2\&prefix=heapster2.\&includeLabels=false\&includeContainers=false
+
 package wavefront
 
 import (
