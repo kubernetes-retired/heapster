cf398c9896976f394c02b614d8c4cbcd6165b517
Jeff Grafton <jgrafton@google.com>
2017-01-10 15:18:06 -0800

    Always --pull in docker build to ensure recent base images

--- kafka/build.sh
+++ kafka/build.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 
 pushd $( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
-docker build -t heapster_kafka:canary .
+docker build --pull -t heapster_kafka:canary .
 popd
--- riemann/build.sh
+++ riemann/build.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 
 pushd $( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
-docker build -t heapster_riemann:canary .
+docker build --pull -t heapster_riemann:canary .
 popd
