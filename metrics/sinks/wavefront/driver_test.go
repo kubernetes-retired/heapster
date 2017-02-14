0abf6b0b2cbe63fcc7454ab64462127b6f4153e6
Evan Pease <evan.pease@gmail.com>
2017-02-14 10:36:09 -0800

    formatted driver_test.go

--- metrics/sinks/wavefront/driver_test.go
+++ metrics/sinks/wavefront/driver_test.go
@@ -70,15 +70,14 @@ func TestName(t *testing.T) {
 	assert.Equal(t, name, "Wavefront Sink")
 }
 
-
 func TestNewMetricName(t *testing.T) {
 	fakeSink := NewFakeWavefrontSink()
 	batch := generateFakeBatch()
 	fakeSink.ExportData(batch)
 	name := "cpu/usage"
 	mtype := "pod_container"
-	newName := fakeSink.cleanMetricName(mtype,name)
-	assert.Equal(t,"pod_container.cpu.usage", newName)
+	newName := fakeSink.cleanMetricName(mtype, name)
+	assert.Equal(t, "pod_container.cpu.usage", newName)
 }
 
 func TestValidateLines(t *testing.T) {
