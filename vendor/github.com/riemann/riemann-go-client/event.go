101b1f9c4b34e891737a37fa782e3807ed3c2704
mcorbin <corbin.math@gmail.com>
2017-02-27 07:51:26 +0100

    Use the new Riemann client in the Riemann sink
    The Riemann sink uses https://github.com/riemann/riemann-go-client
    instead of goryman.
    
    The Riemann sink now sends events to Riemann using TCP. It also adds a
    default tag (`heapster`) to events before sending them.
    
    The new sink sends batch of events to Riemann. The batch size can be
    controlled with a new parameter called "batchsize"

--- vendor/github.com/riemann/riemann-go-client/event.go
+++ vendor/github.com/riemann/riemann-go-client/event.go
@@ -1,16 +1,15 @@
-package goryman
+package riemanngo
 
 import ()
 
-// State is a wrapper for Riemann states
-type State struct {
+// Event is a wrapper for Riemann events
+type Event struct {
 	Ttl         float32
 	Time        int64
 	Tags        []string
 	Host        string // Defaults to os.Hostname()
 	State       string
 	Service     string
-	Once        bool
 	Metric      interface{} // Could be Int, Float32, Float64
 	Description string
 	Attributes  map[string]string
