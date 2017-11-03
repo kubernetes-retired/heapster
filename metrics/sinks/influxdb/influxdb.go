5af9b86220cb789356e39c9ba87425d3b1e96590
Andy Xie <andy.xning@gmail.com>
2017-11-03 10:59:53 +0800

    add concurrency support for metrics influxdb sink

--- metrics/sinks/influxdb/influxdb.go
+++ metrics/sinks/influxdb/influxdb.go
@@ -33,6 +33,10 @@ type influxdbSink struct {
 	sync.RWMutex
 	c        influxdb_common.InfluxdbConfig
 	dbExists bool
+
+	// wg and conChan will work together to limit concurrent influxDB sink goroutines.
+	wg      sync.WaitGroup
+	conChan chan struct{}
 }
 
 var influxdbBlacklistLabels = map[string]struct{}{
@@ -107,7 +111,12 @@ func (sink *influxdbSink) ExportData(dataBatch *core.DataBatch) {
 
 			dataPoints = append(dataPoints, point)
 			if len(dataPoints) >= maxSendBatchSize {
-				sink.sendData(dataPoints)
+				sink.wg.Add(1)
+				// use the channel to block until there's less than the maximum number of concurrent requests running
+				sink.conChan <- struct{}{}
+				go func(dataPoints []influxdb.Point) {
+					sink.sendData(dataPoints)
+				}(dataPoints)
 				dataPoints = make([]influxdb.Point, 0, 0)
 			}
 		}
@@ -160,17 +169,35 @@ func (sink *influxdbSink) ExportData(dataBatch *core.DataBatch) {
 
 			dataPoints = append(dataPoints, point)
 			if len(dataPoints) >= maxSendBatchSize {
-				sink.sendData(dataPoints)
+				sink.wg.Add(1)
+				// use the channel to block until there's less than the maximum number of concurrent requests running
+				sink.conChan <- struct{}{}
+				go func(dataPoints []influxdb.Point) {
+					sink.sendData(dataPoints)
+				}(dataPoints)
 				dataPoints = make([]influxdb.Point, 0, 0)
 			}
 		}
 	}
 	if len(dataPoints) >= 0 {
-		sink.sendData(dataPoints)
+		sink.wg.Add(1)
+		// use the channel to block until there's less than the maximum number of concurrent requests running
+		sink.conChan <- struct{}{}
+		go func(dataPoints []influxdb.Point) {
+			sink.sendData(dataPoints)
+		}(dataPoints)
 	}
+
+	sink.wg.Wait()
 }
 
 func (sink *influxdbSink) sendData(dataPoints []influxdb.Point) {
+	defer func() {
+		// empty an item from the channel so the next waiting request can run
+		<-sink.conChan
+		sink.wg.Done()
+	}()
+
 	if err := sink.createDatabase(); err != nil {
 		glog.Errorf("Failed to create influxdb: %v", err)
 		return
@@ -264,8 +291,9 @@ func new(c influxdb_common.InfluxdbConfig) core.DataSink {
 		glog.Errorf("issues while creating an InfluxDB sink: %v, will retry on use", err)
 	}
 	return &influxdbSink{
-		client: client, // can be nil
-		c:      c,
+		client:  client, // can be nil
+		c:       c,
+		conChan: make(chan struct{}, c.Concurrency),
 	}
 }
 
