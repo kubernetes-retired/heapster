4708a5cc37314176be54a79754e8fccf9e1ac6b1
Jooyoung Jeong <jy190.jung@samsung.com>
2018-01-26 01:21:53 +0000

    apply gofmt

--- metrics/sinks/fluent/driver.go
+++ metrics/sinks/fluent/driver.go
@@ -15,120 +15,120 @@
 package fluent
 
 import (
-        "github.com/golang/glog"
-        fluent "github.com/lestrrat/go-fluent-client"
-        "k8s.io/heapster/metrics/core"
-        "net/url"
-        "strconv"
-        "sync"
+	"github.com/golang/glog"
+	fluent "github.com/lestrrat/go-fluent-client"
+	"k8s.io/heapster/metrics/core"
+	"net/url"
+	"strconv"
+	"sync"
 )
 
 type fluentSink struct {
-        fluent.Client
-        tag string
-        sync.RWMutex
-        fluent.Option
+	fluent.Client
+	tag string
+	sync.RWMutex
+	fluent.Option
 }
 
 type FluentSinkPoint struct {
-        MetricsName      string
-        MetricsValue     interface{}
-        MetricsTags      map[string]string
+	MetricsName  string
+	MetricsValue interface{}
+	MetricsTags  map[string]string
 }
 
 type fluentConfig struct {
-        Host     string
-        Tag      string
-        Buffered bool
+	Host     string
+	Tag      string
+	Buffered bool
 }
 
 func (this *fluentSink) Stop() {
-        // Do nothing.
+	// Do nothing.
 }
 
 func (sink *fluentSink) Name() string {
-        return "Fluent Sink"
+	return "Fluent Sink"
 }
 
 func (sink *fluentSink) ExportData(dataBatch *core.DataBatch) {
-        sink.Lock()
-        defer sink.Unlock()
+	sink.Lock()
+	defer sink.Unlock()
 
-        for _, metricSet := range dataBatch.MetricSets {
-                for metricName, metricValue := range metricSet.MetricValues {
-                        point := FluentSinkPoint{
-                                MetricsName: metricName,
-                                MetricsTags: metricSet.Labels,
-                                MetricsValue: map[string]interface{}{
-                                        "value": metricValue.GetValue(),
-                                },
-                        }
+	for _, metricSet := range dataBatch.MetricSets {
+		for metricName, metricValue := range metricSet.MetricValues {
+			point := FluentSinkPoint{
+				MetricsName: metricName,
+				MetricsTags: metricSet.Labels,
+				MetricsValue: map[string]interface{}{
+					"value": metricValue.GetValue(),
+				},
+			}
 
-                        if err := sink.Post(sink.tag, point, fluent.WithTimestamp(dataBatch.Timestamp.UTC())); err != nil {
-                                glog.Info("failed to post: %s", err)
-                                return
-                        }
-                }
-                for _, metric := range metricSet.LabeledMetrics {
-                        labels := make(map[string]string)
-                        for k, v := range metricSet.Labels {
-                                labels[k] = v
-                        }
-                        for k, v := range metric.Labels {
-                                labels[k] = v
-                        }
-                        point := FluentSinkPoint{
-                                MetricsName: metric.Name,
-                                MetricsTags: labels,
-                                MetricsValue: map[string]interface{}{
-                                        "value": metric.GetValue(),
-                                },
-                        }
+			if err := sink.Post(sink.tag, point, fluent.WithTimestamp(dataBatch.Timestamp.UTC())); err != nil {
+				glog.Info("failed to post: %s", err)
+				return
+			}
+		}
+		for _, metric := range metricSet.LabeledMetrics {
+			labels := make(map[string]string)
+			for k, v := range metricSet.Labels {
+				labels[k] = v
+			}
+			for k, v := range metric.Labels {
+				labels[k] = v
+			}
+			point := FluentSinkPoint{
+				MetricsName: metric.Name,
+				MetricsTags: labels,
+				MetricsValue: map[string]interface{}{
+					"value": metric.GetValue(),
+				},
+			}
 
-                        if err := sink.Post(sink.tag, point, fluent.WithTimestamp(dataBatch.Timestamp.UTC())); err != nil {
-                                glog.Info("failed to post: %s", err)
-                                return
-                        }
-                }
-        }
+			if err := sink.Post(sink.tag, point, fluent.WithTimestamp(dataBatch.Timestamp.UTC())); err != nil {
+				glog.Info("failed to post: %s", err)
+				return
+			}
+		}
+	}
 }
 
 func NewFluentSink(uri *url.URL) (core.DataSink, error) {
-        config := fluentConfig{
-                Host:     "localhost:24224",
-                Tag:      "fluent",
-                Buffered: false,
-        }
+	config := fluentConfig{
+		Host:     "localhost:24224",
+		Tag:      "fluent",
+		Buffered: false,
+	}
 
-        config.Host = uri.Host
-        options := uri.Query()
+	config.Host = uri.Host
+	options := uri.Query()
 
-        // check options
-        if len(options["tag"]) > 0 {
-                config.Tag = options["tag"][0]
-        }
-        if len(options["bufferd"]) > 0 {
-                b, err := strconv.ParseBool(options["bufferd"][0])
-                if err == nil {
-                        config.Buffered = b
-                } else {
-                        glog.Info("failed to convert %s to boolean", options["bufferd"][0], err)
-                        return nil, err
-                }
-        }
+	// check options
+	if len(options["tag"]) > 0 {
+		config.Tag = options["tag"][0]
+	}
+	if len(options["bufferd"]) > 0 {
+		b, err := strconv.ParseBool(options["bufferd"][0])
+		if err == nil {
+			config.Buffered = b
+		} else {
+			glog.Info("failed to convert %s to boolean", options["bufferd"][0], err)
+			return nil, err
+		}
+	}
 
-        client, err := fluent.New(fluent.WithAddress(config.Host), fluent.WithBuffered(config.Buffered))
-        if err != nil {
-                // fluent.New may return an error if invalid values were
-                // passed to the constructor
-                glog.Info("failed to create client: %s", err)
-                return nil, err
-        }
+	client, err := fluent.New(fluent.WithAddress(config.Host), fluent.WithBuffered(config.Buffered))
+	if err != nil {
+		// fluent.New may return an error if invalid values were
+		// passed to the constructor
+		glog.Info("failed to create client: %s", err)
+		return nil, err
+	}
 
-        glog.Info("created fluent sink with options: host:%s tag:%s bufferd:%s", config.Host, config.Tag, config.Buffered)
+	glog.Info("created fluent sink with options: host:%s tag:%s bufferd:%s", config.Host, config.Tag, config.Buffered)
 
-        return &fluentSink{
-                Client: client,
-                tag:    config.Tag,
-        }, nil
+	return &fluentSink{
+		Client: client,
+		tag:    config.Tag,
+	}, nil
 }
